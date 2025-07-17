c24fa36338f2d05a2f94e6d9de542dfd9cf17da6
foreverxxxxxx <emineogutlu3@gmail.com>
2025-07-17 12:23:56 +0300

    Created student table with columns: id, firstName, lastName, and birthOfDate.

---
+++ src/main/java/com/emine/entities/Student.java
@@ -0,0 +1,27 @@
+package com.emine.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name="student")
+@Data //@Getter+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Integer id;
+    @Column(name="first_name", nullable=false)
+   private String firstName;
+    @Column(name="last_name",nullable=false)
+   private String lastName;
+
+    @Column(name="birth_of_name",nullable = true)
+   private Date birthOfDate;
+}
