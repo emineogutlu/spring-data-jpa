25e121ff62e7dc7ebcd352fa499a13e0a57d8d9b
foreverxxxxxx <emineogutlu3@gmail.com>
2025-07-22 21:53:17 +0300

    Add @Repository annotation.

--- src/main/java/com/emine/repository/AddressRepository.java
+++ src/main/java/com/emine/repository/AddressRepository.java
@@ -2,7 +2,9 @@ package com.emine.repository;
 
 import com.emine.entities.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
 }
--- src/main/java/com/emine/repository/HomeRepository.java
+++ src/main/java/com/emine/repository/HomeRepository.java
@@ -2,6 +2,8 @@ package com.emine.repository;
 
 import com.emine.entities.Home;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HomeRepository extends JpaRepository<Home, Long> {
 }
