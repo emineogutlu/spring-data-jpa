89030f64a9df2030e962488ff70d99129914137b
foreverxxxxxx <emineogutlu3@gmail.com>
2025-07-23 03:18:11 +0300

    Refactored property loading by customizing @PropertySource to use app.properties.

--- src/main/java/com/emine/spring_data_jpa/SpringDataJpaApplication.java
+++ src/main/java/com/emine/spring_data_jpa/SpringDataJpaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
@@ -11,7 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = {"com.emine"})
 @ComponentScan(basePackages = {"com.emine"})
 @EnableJpaRepositories(basePackages = {"com.emine"})
-
+@PropertySource(value = "classpath:app.properties")
 
 public class SpringDataJpaApplication {
 
