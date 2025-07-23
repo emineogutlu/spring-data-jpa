80ecbef385f0bb40d373ad1ccd26fc1d8d78824b
foreverxxxxxx <emineogutlu3@gmail.com>
2025-07-23 13:38:09 +0300

    Updated configuration classes for @ConfigurationProperties support.

--- src/main/java/com/emine/configuration/GlobalProperties.java
+++ src/main/java/com/emine/configuration/GlobalProperties.java
@@ -1,19 +1,14 @@
 package com.emine.configuration;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Component
+import java.util.List;
 @Data
+@Component
+@ConfigurationProperties(prefix = "app")
 public class GlobalProperties {
-    @Value("${spring.datasource.url}")
-    private String url;
 
-
-    @Value("${spring.datasource.username}")
-    private String username;
-
-    @Value("${spring.datasource.password}")
-    private String password;
+     private List<Server>servers;
 }
--- src/main/java/com/emine/spring_data_jpa/SpringDataJpaApplication.java
+++ src/main/java/com/emine/spring_data_jpa/SpringDataJpaApplication.java
@@ -1,8 +1,10 @@
 package com.emine.spring_data_jpa;
 
+import com.emine.configuration.GlobalProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -12,7 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = {"com.emine"})
 @ComponentScan(basePackages = {"com.emine"})
 @EnableJpaRepositories(basePackages = {"com.emine"})
-@PropertySource(value = "classpath:app.properties")
+@EnableConfigurationProperties(value= GlobalProperties.class)
 
 public class SpringDataJpaApplication {
 
